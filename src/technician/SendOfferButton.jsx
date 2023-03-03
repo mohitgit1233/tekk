8753ea6fd3667bb04c366363e0072c5926e1b198
Kirito9988 <tthakur01@mylangara.ca>
2023-03-03 04:30:04 -0800

    changes sendoffer

--- src/technician/SendOfferButton.jsx
+++ src/technician/SendOfferButton.jsx
@@ -1,114 +1,61 @@
-import React, { useState } from 'react';
-import { Button, StyleSheet, TextInput, View } from 'react-native';
+import { useState } from 'react';
+import { Button, TextInput, View } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import Moment from 'moment';
-import { Calendar } from 'react-native-calendars';
 
 const SendOffer = ({ route }) => {
-  const { jobId, refreshData, tech_id } = route.params;
+  const { jobId, refreshData,tech_id } = route.params;
   const [offerPrice, setOfferPrice] = useState('');
   const [offerHours, setOfferHours] = useState('');
-  const [selectedDate, setSelectedDate] = useState('');
+  const [preferStartDate, setPreferStartDate] = useState('');
   const navigation = useNavigation();
-
+  console.log('in sending',tech_id)
   const handleSendOffer = () => {
     const offer = {
       jobID: jobId,
       offerPrice: parseInt(offerPrice),
       offerHours: parseInt(offerHours),
-      prefer_start_date: selectedDate,
-      tehnicianId: tech_id,
+      prefer_start_date: preferStartDate,
+      tehnicianId:tech_id
     };
 
     fetch('http://localhost:5001/api/v1/offers', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(offer),
+      body: JSON.stringify(offer)
     })
-      .then((response) => response.json())
-      .then((data) => {
-        navigation.navigate('ViewOffer', { offer: data, refreshData: refreshData });
-      })
-      .catch((error) => console.error(error));
-  };
-
-  const onDayPress = (day) => {
-    setSelectedDate(day.dateString);
+    .then(response => response.json())
+    .then(data => {
+      navigation.navigate('ViewOffer', { offer: data, refreshData: refreshData });
+    })
+    .catch(error => console.error(error));
   };
 
   return (
-    <View style={styles.container}>
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.textInput}
-          placeholder="Offer price"
-          value={offerPrice}
-          onChangeText={setOfferPrice}
-          keyboardType="numeric"
-        />
-      </View>
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.textInput}
-          placeholder="Offer hours"
-          value={offerHours}
-          onChangeText={setOfferHours}
-          keyboardType="numeric"
-        />
-      </View>
+    <View>
       <TextInput
-    style={styles.textInput}
-    placeholder="Select date"
-    value={selectedDate}
-    editable={false}
-  />
-      <Calendar
-        style={styles.calendar}
-        current={Moment().format('YYYY-MM-DD')}
-        minDate={Moment().format('YYYY-MM-DD')}
-        onDayPress={onDayPress}
+        placeholder="Offer price"
+        value={offerPrice}
+        onChangeText={setOfferPrice}
+        keyboardType="numeric"
       />
-      <View style={styles.buttonContainer}>
-        <Button
-          style={styles.button}
-          title="Send Quote"
-          onPress={handleSendOffer}
-          jobId={jobId}
-        />
-      </View>
+      <TextInput
+        placeholder="Offer hours"
+        value={offerHours}
+        onChangeText={setOfferHours}
+        keyboardType="numeric"
+      />
+      <TextInput
+        placeholder="Preferred start date"
+        value={preferStartDate}
+        onChangeText={setPreferStartDate}
+      />
+      <Button
+        title="Send Quote"
+        onPress={handleSendOffer}
+        jobId={jobId}
+      />
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-  },
-  inputContainer: {
-    width: '100%',
-    marginBottom: 20,
-  },
-  textInput: {
-    height: 40,
-    borderColor: '#ccc',
-    borderWidth: 1,
-    borderRadius: 4,
-    paddingHorizontal: 10,
-  },
-  calendar: {
-    marginBottom: 20,
-  },
-  buttonContainer: {
-    width: '100%',
-  },
-  button: {
-    height: 40,
-    backgroundColor: 'blue',
-    color: 'white',
-  },
-});
 
 export default SendOffer;
