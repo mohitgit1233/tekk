bf3cd0c7f4a6b2ef67fcfc5ac83654e9162978ab
Adil Saju <adilsaju@gmail.com>
2023-03-11 04:04:07 -0800

    only flatlist keyExtractor pendinggggg

--- src/client/stacks/AppStackClient.jsx
+++ src/client/stacks/AppStackClient.jsx
@@ -3,13 +3,13 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Ionicons } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import {AspectRatio,Box,Button,HStack,Image,Text,VStack} from 'native-base';
-import SearchFilter from '../../../components/SearchFilter';
+// import SearchFilter from '../../../components/SearchFilter';
 import SearchInput from '../../../components/SearchInput';
 import JobContainer from '../../technician/container/JobContainer';
 import MyPosts from '../myPosts';
 import Offers from '../Offers';
 
-import AllChats from '../allChats';
+import {AllChats} from '../allChats';
 //client account actually (pass a prop) (reusing technician screen)
 import { Account } from '../../technician/screens/Account'
 import AllOffers from '../AllOffers';
@@ -20,6 +20,7 @@ import { Login } from '../../login/Login'
 import NotificationPage from '../../reusable screens/NotificationPage';
 import NotificationBell from '../../reusable screens/NotificationBell';
 
+import  { SubChatClient } from '../SubChatClient'
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -44,6 +45,8 @@ export const AppStackClient = () => {
      <Stack.Screen name='PostDetails' component={PostDetails}></Stack.Screen>
      <Stack.Screen name='Profile' component={TechnicianProfile}></Stack.Screen>
      <Stack.Screen name='Notifications' component={NotificationPage}></Stack.Screen>
+     <Stack.Screen name='SubChatClient' component={SubChatClient}  ></Stack.Screen>
+
 
      {/* <Stack.Screen name='Logout' component={Login}></Stack.Screen> */}
 
