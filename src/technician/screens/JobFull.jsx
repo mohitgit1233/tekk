b5b09fe86bd31e3477489d25d7ea49d91bf28352
Adil Saju <adilsaju@gmail.com>
2023-03-12 14:44:54 -0700

    width jobfull

--- src/technician/screens/JobFull.jsx
+++ src/technician/screens/JobFull.jsx
@@ -119,6 +119,7 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
     elevation: 5,
+    width: '100%', // Set width to stretch to maximum width
   },
   postTitle: {
     fontSize: 20,
