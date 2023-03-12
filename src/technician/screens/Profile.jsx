13e829d1d226638528271099fbe7711fdeec68dc
Kirito9988 <tthakur01@mylangara.ca>
2023-03-11 16:29:05 -0800

    Porfile Image-Error(Adil )

--- src/technician/screens/Profile.jsx
+++ src/technician/screens/Profile.jsx
@@ -46,17 +46,15 @@
 //   );
 // };
 
-
-
-import React, { useState, useEffect } from 'react';
-import { Button, Image, View, Platform } from 'react-native';
+import React, { useState } from 'react';
+import { Button, Image, View } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 
 export const Profile = () => {
   const [image, setImage] = useState(null);
+  const tech_id = '63f17ce257353e03afc8f124';
 
   const pickImage = async () => {
-    // No permissions request is necessary for launching the image library
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.All,
       allowsEditing: true,
@@ -71,11 +69,29 @@ export const Profile = () => {
     }
   };
 
+  const updateProfileImage = async (tech_id, imageUri) => {
+    const formData = new FormData();
+    formData.append('image', {
+      uri: imageUri,
+      name: 'profile-image.jpg',
+      type: 'image/jpeg',
+    });
+    const response = await fetch(`http://localhost:5001/api/v1/technicians/${tech_id}`, {
+      method: 'PUT',
+      body: formData,
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+  };
+
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Button title="Pick an image from camera roll" onPress={pickImage} />
       {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+      {image && <Button title="Submit" onPress={() => updateProfileImage(tech_id, image)} />}
     </View>
   );
-}
-
+};
