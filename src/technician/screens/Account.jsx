aec023a038d3522672fff930f749910bb5ec1bc5
Adil Saju <adilsaju@gmail.com>
2023-03-14 02:07:36 -0700

    account css

--- src/technician/screens/Account.jsx
+++ src/technician/screens/Account.jsx
@@ -1,8 +1,9 @@
-import { AspectRatio, Box, Button, HStack, Image, Text, VStack } from 'native-base';
+import { AspectRatio, Box, Button, HStack, Image, Text, View, VStack } from 'native-base';
 import React from 'react';
 import { TouchableOpacity } from 'react-native';
 // import { Reload } from 'react-native/Libraries/Reload/Reload';
 import { CommonActions } from '@react-navigation/native';
+import { MaterialIcons } from '@expo/vector-icons';
 
 
 
@@ -38,28 +39,43 @@ export const Account = ({ navigation }) => {
   };
 
   return (
-    <VStack flex={1} p={2}>
-      <TouchableOpacity onPress={handleProfilePress}>
-        <Box borderBottomWidth={1} borderBottomColor="gray.300" py={3}>
-          <Text fontSize="lg">Profile</Text>
-        </Box>
-      </TouchableOpacity>
-      {/* <TouchableOpacity onPress={handleJobHistoryPress}>
-        <Box borderBottomWidth={1} borderBottomColor="gray.300" py={3}>
-          <Text fontSize="lg">Job History</Text>
-        </Box>
-      </TouchableOpacity> */}
-      <TouchableOpacity onPress={handleNotificationsPress}>
-        <Box borderBottomWidth={1} borderBottomColor="gray.300" py={3}>
-          <Text fontSize="lg">Notifications</Text>
-        </Box>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={handleLogoutPress}>
-        <Box borderBottomWidth={1} borderBottomColor="gray.300" py={3}>
-          <Text fontSize="lg" color="red.500">Logout</Text>
-        </Box>
-      </TouchableOpacity>
-    </VStack>
+
+<VStack flex={1} p={2} style={{backgroundColor:"white"}}>
+  <TouchableOpacity onPress={handleProfilePress}>
+    <Box borderBottomWidth={1} borderBottomColor="gray.300" py={3}>
+      <HStack alignItems="center">
+        <MaterialIcons name="person" size={24} color="black" style={{ marginRight: 10 }} />
+        <Text fontSize="lg">Profile</Text>
+      </HStack>
+    </Box>
+  </TouchableOpacity>
+  {/* <TouchableOpacity onPress={handleJobHistoryPress}>
+    <Box borderBottomWidth={1} borderBottomColor="gray.300" py={3}>
+      <HStack alignItems="center">
+        <MaterialIcons name="work" size={24} color="black" style={{ marginRight: 10 }} />
+        <Text fontSize="lg">Job History</Text>
+      </HStack>
+    </Box>
+  </TouchableOpacity> */}
+  <TouchableOpacity onPress={handleNotificationsPress}>
+    <Box borderBottomWidth={1} borderBottomColor="gray.300" py={3}>
+      <HStack alignItems="center">
+        <MaterialIcons name="notifications" size={24} color="black" style={{ marginRight: 10 }} />
+        <Text fontSize="lg">Notifications</Text>
+      </HStack>
+    </Box>
+  </TouchableOpacity>
+  <TouchableOpacity onPress={handleLogoutPress}>
+    <Box borderBottomWidth={1} borderBottomColor="gray.300" py={3}>
+      <HStack alignItems="center">
+        <MaterialIcons name="logout" size={24} color="red" style={{ marginRight: 10 }} />
+        <Text fontSize="lg" color="red.500">Logout</Text>
+      </HStack>
+    </Box>
+  </TouchableOpacity>
+</VStack>
+
+
   );
 };
 
