8333846d83f4b5383800158ad561ea048f61dead
Adil Saju <adilsaju@gmail.com>
2023-03-16 16:50:57 -0700

    added ai menu

--- src/client/allChats.jsx
+++ src/client/allChats.jsx
@@ -34,6 +34,18 @@ export const AllChats = ({navigation}) => {
   return (
     <View style={styles.container}>
       <Text style={styles.head}>Select Employer To Chat</Text>
+            {/* AI button */}
+            <TouchableOpacity
+            style={styles.postContainer}
+          >
+            <Image
+              source={{ uri: "https://fastly.picsum.photos/id/1/200/300.jpg?hmac=jH5bDkLr6Tgy3oAg5khKCHeunZMHq0ehBZr6vGifPLY" }}
+              style={styles.image}
+            />
+            <Text style={styles.postDescription}>
+              Ask AI
+            </Text>
+          </TouchableOpacity>
       <FlatList
         style={styles.list}
         data={data1}
