b3fa7a36661e269ec23b832aab05784d2b0551b3
Adil Saju <adilsaju@gmail.com>
2023-03-17 00:07:53 -0700

    create post all error fix & loader

--- src/client/CreatePost.jsx
+++ src/client/CreatePost.jsx
@@ -1,6 +1,6 @@
 import { useState,useContext,useEffect } from 'react';
 import {Box} from 'native-base'
-import { Button, TextInput, View,StyleSheet,Text,Image,FlatList , ScrollView, TouchableOpacity} from 'react-native';
+import { Button, TextInput, View,StyleSheet,Text,Image,FlatList , ScrollView, TouchableOpacity, ActivityIndicator} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 import DateTimePicker from '@react-native-community/datetimepicker';
@@ -23,18 +23,21 @@ const CreatePost = () => {
     console.log("getGeneratedAd()");
 
     try {
-        
-        const data = await getCompletionsOpenAI(null, {
-            message: message,
-          })
-    console.log("raaaaaaaaaaaaaaaaaaaaaaaaaaaa",data);
+      setLoading(true);
 
-        const completions = data.choices.map((choice) => choice.text.trim());
-        // setSuggestedReplies(completions[0]);
-        setJobDescription(completions[0])
-      } catch (error) {
-        console.error(error);
-      }
+      const data = await getCompletionsOpenAI(null, {
+        message: message,
+      });
+
+      console.log("raaaaaaaaaaaaaaaaaaaaaaaaaaaa", data);
+
+      const completions = data.choices.map((choice) => choice.text.trim());
+      setJobDescription(completions[0]);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
   };
 
   useEffect(() => {
@@ -73,6 +76,8 @@ const CreatePost = () => {
     const [startDate,setStartDate] = useState(new Date())
     const [requirement,setRequirement] = useState(null)
     const [jobDescription,setJobDescription] = useState('')
+    const [loading, setLoading] = useState(false);
+
     const [images,setImages] = useState([])
     const [open, setOpen] = useState(false);
 
@@ -140,265 +145,253 @@ const CreatePost = () => {
       navigation.goBack();
     };
     return (
-      <ScrollView >
-      <ScrollView contentContainerStyle={styles.container}>
-        <View style={styles.form}>
-
-    
-          <View style={styles.inputContainer}>
-            <View style={styles.labelinputwrapper}>  
-            
-            <TextInput
-              placeholder="Job Title"
-              value={postTitle}
-              style={styles.input}
-              onChangeText={setPostTitle}
-            />
-            </View>
-          
-            <View style={styles.labelinputwrapper}>  
-        
-            <TextInput
-              placeholder="Address"
-              value={postAddress}
-              style={styles.input}
-              onChangeText={setPostAddress}
-            />
-            </View> 
-
-
-            <View style={styles.labelinputwrapper}>   
-        
-            <TextInput
-              placeholder="Phone Number"
-              value={postPhone}
-              style={styles.input}
-              onChangeText={setPostPhone}
-              keyboardType="numeric"
-            />
-          </View>
-
-
-          <View style={styles.labelinputwrapper}>  
-      
-            <TextInput
-              placeholder="Maximum Cost"
-              value={postMaxCost}
-              style={styles.input}
-              onChangeText={setPostMaxCost}
-            />
-
-            </View>
-            <View style={styles.labelinputwrapper}>  
- 
-            <View style={styles.container1}>
-      <TextInput
-        placeholder="Description"
-        value={jobDescription}
-        style={styles.description}
-        onChangeText={setJobDescription}
-        multiline={true}
-        numberOfLines={4}
-      />
-      <TouchableOpacity style={styles.fillButton} onPress={()=>{getGeneratedAd(`Create a 500-word ad description to help me hire a technician based on this: title: ${postTitle}, job: ${requirement}, address: ${postAddress}, payment: ${postMaxCost}, start-date: ${startDate}`)}}>
-        <Text style={{ color: 'white' }}>Auto Generate</Text>
-      </TouchableOpacity>
-    </View>
-
-
-
-
-          </View>
-            
-            <TouchableOpacity
-              style={styles.input}
-              onPress={() => setShowPicker(true)}
-            >
-              <Text style={styles.inputLabel}>Preferred Start Date</Text>
-              {showPicker && (
-                <View style={{display:'flex',justifyContent:'center'}}>
-                  <DateTimePicker
-                  style={{marginRight:100,marginTop:6,marginBottom:6,borderBottomWidth:0}}
-                  value={startDate}
-                  mode="date"
-                  onChange={onDateChange}
+      <FlatList
+        contentContainerStyle={styles.container}
+        data={[1]}
+        keyExtractor={() => 'dummy-key'}
+        renderItem={() => (
+          <View style={styles.form}>
+            <View style={styles.inputContainer}>
+              <View style={styles.labelinputwrapper}>
+                <TextInput
+                  placeholder="Job Title"
+                  value={postTitle}
+                  style={styles.input}
+                  onChangeText={setPostTitle}
                 />
-                </View>
-                
-              )}
-            </TouchableOpacity>
-
-            
-
-  
-
-            
-            <View style={styles.labelinputwrapper}>  
-
-           
-            <DropDownPicker
-              style={styles.dropdown}
-              open={open}
-              value={requirement}
-              items={items}
-              setOpen={setOpen}
-              setValue={setRequirement}
-              setItems={setItems}
-              placeholder="Select a requirement"
+              </View>
+      
+              <View style={styles.labelinputwrapper}>
+                <TextInput
+                  placeholder="Address"
+                  value={postAddress}
+                  style={styles.input}
+                  onChangeText={setPostAddress}
+                />
+              </View>
+      
+              <View style={styles.labelinputwrapper}>
+                <TextInput
+                  placeholder="Phone Number"
+                  value={postPhone}
+                  style={styles.input}
+                  onChangeText={setPostPhone}
+                  keyboardType="numeric"
+                />
+              </View>
+      
+              <View style={styles.labelinputwrapper}>
+                <TextInput
+                  placeholder="Maximum Cost"
+                  value={postMaxCost}
+                  style={styles.input}
+                  onChangeText={setPostMaxCost}
+                />
+              </View>
+      
+              <View style={styles.labelinputwrapper}>
+              <View style={styles.container1}>
+            <TextInput
+              placeholder="Description"
+              value={jobDescription}
+              style={styles.description}
+              onChangeText={setJobDescription}
+              multiline={true}
+              numberOfLines={4}
             />
-            </View>
+            <TouchableOpacity style={styles.fillButton} onPress={() => getGeneratedAd(`Create a 500-word ad description to help me hire a technician based on this: title: ${postTitle}, job: ${requirement}, address: ${postAddress}, payment: ${postMaxCost}, start-date: ${startDate}`)}>
+              {loading ? <ActivityIndicator size="small" color="#FFFFFF" /> : <Text style={{ color: 'white' }}>Auto Generate</Text>}
+            </TouchableOpacity>
           </View>
-        
-          {/* <View style={styles.imageinputwrapper}>   */}
+              </View>
+      
+              <TouchableOpacity
+                style={styles.input}
+                onPress={() => setShowPicker(true)}
+              >
+                <Text style={styles.inputLabel}>Preferred Start Date</Text>
+                {showPicker && (
+                  <View style={{ display: 'flex', justifyContent: 'center' }}>
+                    <DateTimePicker
+                      style={{ marginRight: 100, marginTop: 6, marginBottom: 6, borderBottomWidth: 0 }}
+                      value={startDate}
+                      mode="date"
+                      onChange={onDateChange}
+                    />
+                  </View>
+                )}
+              </TouchableOpacity>
+      
+              <View style={styles.labelinputwrapper}>
+                <DropDownPicker
+                  style={styles.dropdown}
+                  open={open}
+                  value={requirement}
+                  items={items}
+                  setOpen={setOpen}
+                  setValue={setRequirement}
+                  setItems={setItems}
+                  placeholder="Select a requirement"
+                  dropDownDirection="up"
+                  dropDownContainerStyle={styles.dropDownContainer}
+                />
+              </View>
+            </View>
+      
             <TouchableOpacity style={styles.addImagesButton} onPress={pickImages}>
-              <Text style={{color:'white',textAlign:'center'}} >Add Site Images</Text>
+              <Text style={{ color: 'white', textAlign: 'center' }}>Add Site Images</Text>
             </TouchableOpacity>
             <FlatList
-      horizontal={true}
-  data={images}
-  renderItem={({ item }) => (
-    <Image source={{ uri: item }} style={{ width: 200, height: 200 }} />
-  )}
-  keyExtractor={(item) => item}
-/>
-            {/* </View> */}
-    
-          <TouchableOpacity style={styles.botton} onPress={handleSendOffer}>
-            <Text style={styles.btntxt}>Submit Post</Text>
-          </TouchableOpacity>
-        </View>
-      </ScrollView>
-      </ScrollView>
-    );
-}
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 10,
-  },
-  formContainer: {
-    backgroundColor: '#FFFFFF',
-    borderRadius: 8,
-    padding: 16,
-  },
-  label: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginBottom: 8,
-  },
-  input: {
-    borderBottomWidth:1,
-    borderColor: '#404040',
-    borderRadius: 8,
-    paddingHorizontal: 16,
-    paddingVertical: 12,
-    marginBottom: 16,
-    fontSize: 16,
-  },
-  description: {
-    borderWidth:1,
-    borderColor: '#404040',
-    height:120,
-    borderRadius: 8,
-    paddingHorizontal: 16,
-    paddingVertical: 12,
-    marginBottom: 16,
-    fontSize: 16,
-  },
-  button: {
-    backgroundColor: '#5D2DFD',
-    padding: 16,
-    borderRadius: 8,
-    alignItems: 'center',
-  },
-  buttonText: {
-    color: '#FFFFFF',
-    fontSize: 16,
-  },
-  datePickerContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    borderWidth: 1,
-    borderColor: '#CCCCCC',
-    borderRadius: 8,
-    paddingHorizontal: 16,
-    paddingVertical: 12,
-    marginBottom: 16,
-  },
-  dateText: {
-    fontSize: 16,
-  },
-  dropdownContainer: {
-    marginBottom: 16,
-  },
-  imageinputwrapper:{
-    display:'flex',
-    justifyContent:'center',
-    alignItems:'center',
-    borderWidth:1,
-    borderStyle:'dashed',
-    borderColor: '#404040',
-    height:150,
-    marginTop: 20,
-    marginBottom: 20,
-    fontSize: 16,
-  }, 
-  botton:{
-    backgroundColor: '#0D937D',
-    paddingVertical: 15,
-    paddingHorizontal: 100,
-    // borderRadius: 30,
-    marginTop: 20,
-    marginBottom: 20,
-  },
-  btntxt:{
-    color: '#FFFFFF',
-    fontSize: 15,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-  addImagesButton:{
-   
-    backgroundColor: '#0D937D',
-    // paddingVertical: 15,
-    // paddingHorizontal: 100,
-    // borderRadius: 30,
-    marginTop: 10,
-    marginBottom: 10,
-    padding:12,
-
-    width:"50%",
-    textAlign:'center',
-    alignSelf: 'center',
-    // marginTop:50
-  },
-  // dropdown: {
-  //   backgroundColor:"#0D937D",
-  //   color: '#FFFFFF',
-  // }
-
-  container1: {
-    position: 'relative',
-  },
-  description: {
-    height: 100,
-    padding: 10,
-    borderWidth: 1,
-    borderColor: 'gray',
-  },
-  fillButton: {
-    position: 'absolute',
-    top: 10,
-    right: 10,
-    backgroundColor: 'gray',
-    borderRadius: 5,
-    padding: 5,
-    backgroundColor: "#56565661"
-  },
-
-  
-});
-
-
-export default CreatePost
+              horizontal={true}
+              data={images}
+              renderItem={({ item }) => (
+                <Image source={{ uri: item }} style={{ width: 200, height: 200 }} />
+              )}
+              keyExtractor={(item) => item}
+            />
+      
+            <TouchableOpacity style={styles.botton} onPress={handleSendOffer}>
+              <Text style={styles.btntxt}>Submit Post</Text>
+            </TouchableOpacity>
+          </View>
+        )}
+      />
+      
+        );
+      }
+      const styles = StyleSheet.create({
+        container: {
+          // flex: 1,
+          padding: 10,
+        },
+        formContainer: {
+          backgroundColor: '#FFFFFF',
+          borderRadius: 8,
+          padding: 16,
+        },
+        label: {
+          fontSize: 18,
+          fontWeight: 'bold',
+          marginBottom: 8,
+        },
+        input: {
+          borderBottomWidth: 1,
+          borderColor: '#404040',
+          borderRadius: 8,
+          paddingHorizontal: 16,
+          paddingVertical: 12,
+          marginBottom: 16,
+          fontSize: 16,
+        },
+        description: {
+          borderWidth: 1,
+          borderColor: '#404040',
+          height: 120,
+          borderRadius: 8,
+          paddingHorizontal: 16,
+          paddingVertical: 12,
+          marginBottom: 16,
+          fontSize: 16,
+        },
+        button: {
+          backgroundColor: '#5D2DFD',
+          padding: 16,
+          borderRadius: 8,
+          alignItems: 'center',
+        },
+        buttonText: {
+          color: '#FFFFFF',
+          fontSize: 16,
+        },
+        datePickerContainer: {
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          borderWidth: 1,
+          borderColor: '#CCCCCC',
+          borderRadius: 8,
+          paddingHorizontal: 16,
+          paddingVertical: 12,
+          marginBottom: 16,
+        },
+        dateText: {
+          fontSize: 16,
+        },
+      
+        imageinputwrapper: {
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          borderWidth: 1,
+          borderStyle: 'dashed',
+          borderColor: '#404040',
+          height: 150,
+          marginTop: 20,
+          marginBottom: 20,
+          fontSize: 16,
+        },
+        botton: {
+          backgroundColor: '#0D937D',
+          paddingVertical: 15,
+          paddingHorizontal: 100,
+          // borderRadius: 30,
+          marginTop: 20,
+          marginBottom: 20,
+        },
+        btntxt: {
+          color: '#FFFFFF',
+          fontSize: 15,
+          fontWeight: 'bold',
+          textAlign: 'center',
+        },
+        addImagesButton: {
+      
+          backgroundColor: '#0D937D',
+          // paddingVertical: 15,
+          // paddingHorizontal: 100,
+          // borderRadius: 30,
+          marginTop: 10,
+          marginBottom: 10,
+          padding: 12,
+      
+          width: "50%",
+          textAlign: 'center',
+          alignSelf: 'center',
+          // marginTop:50
+        },
+        dropdown: {
+          zIndex: 999,
+          width: '100%',
+          marginBottom: 10,
+        },
+        dropDownContainer: {
+          height: 150,
+          width: '100%',
+          position: 'absolute',
+          top: -150,
+          zIndex: 999,
+        },
+        container1: {
+          position: 'relative',
+        },
+        description: {
+          height: 100,
+          padding: 10,
+          borderWidth: 1,
+          borderColor: 'gray',
+        },
+        fillButton: {
+          position: 'absolute',
+          top: 10,
+          right: 10,
+          backgroundColor: 'gray',
+          borderRadius: 5,
+          padding: 5,
+          backgroundColor: "#56565661"
+        },
+      
+      
+      });
+      
+      
+      export default CreatePost
