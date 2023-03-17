b43d7d7b532385eb941e7844a64b2aac29195d43
Adil Saju <adilsaju@gmail.com>
2023-03-17 13:10:11 -0700

    SubChat Client side Chat CSS

--- src/client/SubChatClient.jsx
+++ src/client/SubChatClient.jsx
@@ -1,19 +1,15 @@
 
 import React, { useState, useEffect,useContext } from 'react';
-import { StyleSheet, View, TextInput, Button, FlatList, Text, KeyboardAvoidingView } from 'react-native';
+import { StyleSheet, View, TextInput, Button, FlatList, Text, KeyboardAvoidingView, ScrollView,Image, TouchableOpacity } from 'react-native';
 import io from 'socket.io-client';
-import { getMessages,getUserById } from '../../services/api';
+import { getMessages,getUserById, getCompletionsOpenAI, getJobById } from '../../services/api';
 import { SOCKET_API } from '../../services/api_config';
 import AppContext from '../../AppContext';
 import Moment from 'moment';
 
-// const connection_api = 'http://192.168.5.131:3000/connection';
-// const message_api = 'http://192.168.5.131:3000/message';
-// const socket_api = 'http://localhost:5001'
-
 export const SubChatClient = ({ navigation, route }) => {
     const { loggedInUser, setLoggedInUser } = useContext(AppContext);
-    const { propValue, p2, roomid } = route.params;
+    const { propValue, p2, roomid, job_id } = route.params;
 
     const [tomessage, set_tomessage] = useState('');
     const [messages, setMessages] = useState([]);
@@ -22,32 +18,46 @@ export const SubChatClient = ({ navigation, route }) => {
     const [tech_id, setTech_id] = useState(loggedInUser.id);
     const [tech_name, settech] = useState("");
     const [emp_name, setemp] = useState("");
+    const [jobn, setjobn] = useState("");
+    //aii sugg
+    const [lastMessage, setLastMessage] = useState(null);
+    const [suggestedReplies, setSuggestedReplies] = useState(["hello","okay","thank you", "what's the pay"]);
+
 
     //   const [socket, setSocket] = useState(null);
     const socket = io.connect(SOCKET_API);
 
+    const getSuggestedReplies = async (message) => {
+        console.log("getSuggestedReplies()");
+
+        try {
+            
+            const data = await getCompletionsOpenAI(null, {
+                message: `What's the best reply to "${message}"?(give a max 4-word answer)`,
+              })
+        console.log("raaaaaaaaaaaaaaaaaaaaaaaaaaaa",data);
+
+            const completions = data.choices.map((choice) => choice.text.trim());
+            setSuggestedReplies(completions);
+          } catch (error) {
+            console.error(error);
+          }
+      };
 
     useEffect(() => {
         //populate messages using database
         const see = async()=>{
-        //     await fetch('http://localhost:5001/api/v1/messages')
-        //   .then((resp) => resp.json())
-        //   .then((json) => {
-            
-        //     // setData(json)
-        //     setMessages(json)
-        //     // setMessages(messages => [...messages, json[0]  ]);
-        //     console.log("speciallllll===============================l");
-        //     console.log(json);
-        
-        // })
-        //   .catch((error) => console.error(error));
+
         const json = await getMessages(roomid)
-        const tech_name = await getUserById(p2)
         const emp_name = await getUserById(tech_id)
-        settech(tech_name.name)
-        setemp(emp_name.name)
+        const tech_name = await getUserById(p2)
+        const jobi = await  getJobById(job_id)
+        settech(tech_name)
+        setemp(emp_name)
+        setjobn(jobi)
         setMessages(json)
+        setLastMessage(json[json.length-1]&& json[json.length-1].message);
+        getSuggestedReplies(json[json.length-1]&&json[json.length-1].message);
         }
         see()
 
@@ -69,6 +79,9 @@ export const SubChatClient = ({ navigation, route }) => {
             console.log("msg list");
             console.log(messages);
 
+            //recieve bubble fix
+            getSuggestedReplies(messages[messages.length-1]&&messages[messages.length-1].message);
+
             //populate (auto in react)
             // update();
         });
@@ -79,22 +92,6 @@ export const SubChatClient = ({ navigation, route }) => {
 
     }, []);
 
-    // const connectApi = async () => {
-    //     try {
-
-    //         const response0 = await fetch(connection_api, {
-    //             method: 'POST',
-    //             headers: {
-    //                 'Content-Type': 'application/json',
-    //             }
-    //         });
-    //         const data0 = await response0.json();
-    //         console.log("init success");
-
-    //     } catch (error) {
-    //         console.error(error);
-    //     }
-    // }
 
     function joinRoom() {
         console.log("guy joining");
@@ -126,6 +123,10 @@ export const SubChatClient = ({ navigation, route }) => {
             //populate
 
             set_tomessage('');
+            setLastMessage(tomessage);
+            // await getSuggestedReplies(lastMessage);
+            await getSuggestedReplies(tomessage);
+
 
             //   setMessages(prevMessages => [...prevMessages, { id: Date.now(), message: data1.output.trim(), sender: "Chatgpt" }]);
         } catch (error) {
@@ -135,12 +136,17 @@ export const SubChatClient = ({ navigation, route }) => {
 
     const renderItem = ({ item }) => {
         return (
-            <View style={styles.message}>
+            item.docModel==="employer" ?            ( <View style={styles.message2}>
                 {/* <Text style={styles.sender} >{item.sender_id}</Text> */}
                 <Text style={styles.sender}>{Moment(item.date).format('MMMM Do, YYYY,HH:mm A')}</Text>
-                { item.docModel==="technician" ? <Text style={styles.sender} >{tech_name}</Text> : <Text style={styles.sender} >{emp_name}</Text>  }
+               {/* <Text style={styles.sender} >{tech_name.name}</Text>  */}
                 <Text>{item.message}</Text>
-            </View>
+            </View>) :          (   <View style={styles.message1}>
+            {/* <Text style={styles.sender} >{item.sender_id}</Text> */}
+            <Text style={styles.sender}>{Moment(item.date).format('MMMM Do, YYYY,HH:mm A')}</Text>
+           {/* <Text style={styles.sender} >{emp_name.name}</Text>  */}
+            <Text>{item.message}</Text>
+        </View>)  
         );
     };
 
@@ -151,19 +157,21 @@ export const SubChatClient = ({ navigation, route }) => {
             keyboardVerticalOffset={120} // adjust this value as needed
         >
             <View style={styles.container}>
-                {/* <TextInput
-                    placeholder="Tsaddsadsdsa..."
-                    value={tech_id}
-                    onChangeText={(text) => setTech_id(text)}
-                /> */}
-                {/* <Text style={{ textAlign: "center", padding: "3%" }}>Job Id: {propValue}</Text> */}
-                <Text style={{ textAlign: "center", padding: "3%" }}>Technician: {p2}</Text>
-                <Text style={{ textAlign: "center", padding: "3%" }}>Employer Currently logged in: {tech_id}</Text>
-                <Text style={{ textAlign: "center", padding: "3%" }}>ROOM ID: {roomid}</Text>
+            <View style={styles.header}>
+            <Image
+              source={{ uri: "https://picsum.photos/200" }}
+              style={styles.image}
+            />
+            <View style={styles.headerText}>
+              <Text style={styles.roomId}>{jobn.title}</Text>
+              <Text style={styles.employer}>Technician: {emp_name.name}</Text>
+            </View>
+          </View>
 
 
 
                 <FlatList
+                style={{marginBottom:50}}
                     data={messages}
                     renderItem={renderItem}
                     keyExtractor={(item, index) => index.toString()}
@@ -177,29 +185,111 @@ export const SubChatClient = ({ navigation, route }) => {
                         multiline={true}
                     // onSubmitEditing={handleSend}
                     />
-                    <Button
+                    {/* <Button
                         title="Send"
                         onPress={handleSend}
-                    />
+                    /> */}
+                                        <TouchableOpacity style={styles.sendButton} onPress={handleSend}>
+              <Text style={styles.sendButtonText}>Send</Text>
+            </TouchableOpacity>
                 </View>
+                {suggestedReplies.length > 0 && (
+                    <View style={styles.suggestionList}>
+
+<ScrollView horizontal showsHorizontalScrollIndicator={false}>
+
+
+                        {suggestedReplies.map((reply) => (
+                            <Text
+                            style={styles.suggestion}
+                            onPress={() => set_tomessage(reply)}
+                            
+                            >
+                                {reply}
+                            </Text>
+                        ))}
+                        </ScrollView>
+                    </View>
+                )}
+
+
             </View>
         </KeyboardAvoidingView>
     );
 };
 
 const styles = StyleSheet.create({
+    // container: {
+    //     flex: 1,
+    //     backgroundColor: '#fff',
+    //     padding: 10,
+    // },
     container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        padding: 10,
+      flex: 1,
+      backgroundColor: "#F5F5F5",
+      padding: 10,
     },
-    message: {
+    header: {
+      flexDirection: "row",
+      alignItems: "center",
+      marginBottom: 10,
+      backgroundColor: "#FFFFFF",
+      padding: 10,
+      borderRadius: 10,
+    },
+    headerText: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    image: {
+      width: 60,
+      height: 60,
+      borderRadius: 30,
+      marginRight: 10,
+    },
+    roomId: {
+      fontSize: 18,
+      fontWeight: "bold",
+      marginBottom: 5,
+      alignItems: 'flex-start',
+      alignSelf: 'flex-start'
+    },
+    employer: {
+      fontSize: 18,
+      fontWeight: "bold",
+      marginBottom: 5,
+      alignItems: 'flex-start',
+      alignSelf: 'flex-start'
+      
+    },
+    message1: {
         padding: 10,
+        maxWidth: "70%",
+        alignSelf: "flex-start",
         borderWidth: 1,
-        borderColor: '#ccc',
-        borderRadius: 5,
+        borderColor: "#ccc",
+        borderRadius: 10,
         marginBottom: 5,
+        borderTopLeftRadius: 2,
+        backgroundColor: "#EBEBEB",
     },
+    message2: {
+          backgroundColor: "#DCF8C5",
+        padding: 10,
+        maxWidth: "70%",
+        alignSelf: "flex-end",
+        borderWidth: 1,
+        borderColor: "#ccc",
+        borderRadius: 10,
+        marginBottom: 5,
+        borderTopRightRadius: 2,
+      },
+      sender: {
+        fontSize: 12,
+        color: "grey",
+        marginTop: 5,
+      },
     inputContainer: {
         flexDirection: 'row',
         alignItems: 'center',
@@ -216,9 +306,44 @@ const styles = StyleSheet.create({
         paddingHorizontal: 10,
         marginRight: 10,
     },
-    sender: {
-        // backgroundColor: "red"
-        color: "grey"
-    }
+    suggestionList: {
+            backgroundColor: 'rgba(255, 255, 255, 0.8)', 
+        // backgroundColor: "#f2f2f2",
+        padding: 15,
+        marginBottom: 10,
+        position: "absolute",
+        bottom: 50,
+        left: 0,
+        right: 0,
+        flexDirection: "row",
+        flexWrap: "wrap",
+        justifyContent: "center",
+        alignItems: "center",
+      },
+      suggestion: {
+        backgroundColor: "#e0e0e0",
+        color: "#333",
+        paddingVertical: 5,
+        paddingHorizontal: 10,
+        borderRadius: 20,
+        marginHorizontal: 5,
+        marginVertical: 2,
+      },
+      // image: {
+      //   width: 60,
+      //   height: 60,
+      //   borderRadius: 30,
+      //   marginRight: 10,
+      // },
+      sendButton: {
+        backgroundColor: "#0D937D",
+        borderRadius: 5,
+        paddingVertical: 10,
+        paddingHorizontal: 15,
+      },
+      sendButtonText: {
+        color: "#FFFFFF",
+        fontWeight: "bold",
+      },
 });
 
