bba309e70c83a9a768180fb68c6cce3cc13d0e0b
Adil Saju <adilsaju@gmail.com>
2023-03-20 02:39:26 -0700

    cost numeric input

--- src/client/CreatePost.jsx
+++ src/client/CreatePost.jsx
@@ -187,6 +187,7 @@ const CreatePost = () => {
                   value={postMaxCost}
                   style={styles.input}
                   onChangeText={setPostMaxCost}
+                  keyboardType="numeric"
                 />
               </View>
       
